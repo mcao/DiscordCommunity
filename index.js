6604d582808875b2c039f0f337c1fefdfe7af62c
Michael Cao <mike.x.cao@gmail.com>
2017-12-18 18:03:48 -0500

    some code

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-const Eris = require("eris");
+const Eris = require("eris"),
+    config = require("./config.json")
+var bot = new Eris(config.token);
+
+bot.on("ready", () => {
+    console.log("Ready!");
+});
+
+bot.on("messageCreate", (msg) => {
+    if (msg.content === "!ping") {
+        bot.createMessage(msg.channel.id, "Pong!");
+    }
+});
+
+bot.connect();
