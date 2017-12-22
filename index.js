95ab09606c1ac31dc0d104dd5a3d8a7085042bf6
Michael Cao <mike.x.cao@gmail.com>
2017-12-22 17:01:18 -0500

    Welcome msg crap

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ bot.on("ready", () => {
 
 bot.on("guildMemberAdd", function (guild, member) {
     if (guild.id == "358528040617377792") {
-        bot.createMessage("392152516596465664", `Welcome ${member.user} to !!${guild.name}!** :tada::tada:`)
+        bot.createMessage("392152516596465664", `Welcome ${member.user} to **${guild.name}!** :tada::tada:`)
     }
 })
 
