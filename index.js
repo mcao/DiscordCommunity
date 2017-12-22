c7078c25e9e0ea41ac69b9aa2803e2212b35f0f0
Michael Cao <mike.x.cao@gmail.com>
2017-12-22 16:55:10 -0500

    Unban Sync

--- index.js
+++ index.js
@@ -35,12 +35,29 @@ bot.on("guildBanAdd", function (guild, user) {
                     }
                 }
                 guild.getBans(user.id).then(thisBans => {
-                    bot.getDMChannel(user.id).then(chan => {
-                        bot.createMessage(chan.id, `You have been banned from ${guild2.name} for ${thisBans[0].reason}!`)
-                    })
                     console.log(`Banning ${user.username} on ${guild2.name}!`)
                     guild2.banMember(user.id, 0, "Automated Ban Sync - User banned on " + guild.name + " for " + thisBans[0].reason)
                 });
+            })
+        } catch (err) {
+            bot.getChannel("389588585889660928").createMessage(`Error: ${err}`)
+        }
+    }
+});
+
+bot.on("guildBanRemove", function (guild, user) {
+    const guildList = bot.config.guilds;
+
+    for (var i = 0; i < guildList.length; i++) {
+        try {
+            const guild2 = bot.guilds.get(guildList[i])
+            guild2.getBans().then(thatBans => {
+                for (var j = 0; j < thatBans.length; j++) {
+                    if (thatBans[j].user.id == user.id) {
+                        console.log(`Unbanning ${user.username} on ${guild2.name}!`)
+                        guild2.unbanMember(user.id, 0, "Automated Unban Sync - User unbanned on " + guild.name)
+                    }
+                }
             })
         } catch (err) {
             bot.getChannel("389588585889660928").createMessage(`Error: ${err}`)
