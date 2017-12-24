9d6c5c87baa392a7a98e5a24fb73796ce1f6aa44
Unknown <justmesaif@yahoo.com>
2017-12-24 16:27:00 -0500

    fix #4

--- modules/levels.js
+++ modules/levels.js
@@ -24,7 +24,7 @@ module.exports = (bot) => {
         bot.register("reset", (msg, args) => {
             var user = msg.mentions[0].id;
             if (args.length === 0) return bot.createMessage(msg.channel.id, 'Please provide a user.');
-            if (msg.mentions.bot) return bot.createMessage(msg.channel.id, 'Bots don\'t have ranks!.');
+            if (msg.mentions[0].bot) return bot.createMessage(msg.channel.id, 'Bots don\'t have ranks!.');
             if (!msg.mentions[0] && args.length !== 18) return bot.createMessage(msg.channel.id, 'Invalid user.')
             if (args.length === 18 && typeof args === 'number') user = args;
             const levelRoles = ['393606932608450561', '393606931014746113', '393606929068589057', '393606926467989507', '393606924433752064'];
