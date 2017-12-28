3f869638e7e1ced20623699aeb2c881ae8e20b88
JoeBanks13 <joseph@josephbanks.me>
2017-12-28 20:47:07 +0000

    whoops syntax

--- modules/levels.js
+++ modules/levels.js
@@ -76,7 +76,7 @@ module.exports = (bot) => {
                     response: (msg, args) => {
 			if(msg.reactions.length == 1){
 				return msg.content
-			})
+			}
 			msg.getReaction("▶").then((users) => { users.forEach((v) => { if(v.id != bot.user.id){ msg.removeReaction("▶", v.id)}})});
                         var i = (msg.content.substring(msg.content.indexOf("Page") + 5, msg.content.indexOf("Page") + 6) * 1)
                         var start = i * 20;
