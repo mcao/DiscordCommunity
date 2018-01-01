938780b37b6e5f90897f24453e0e1e04e1f370fe
JoeBanks13 <joseph@josephbanks.me>
2018-01-01 04:36:15 +0000

    more testing

--- modules/moderation.js
+++ modules/moderation.js
@@ -84,16 +84,17 @@ module.exports = (bot) => {
                             }
 
                         });
+                        setTimeout(() => {
+                            if (reacted == false) {
+                                bot._events.messageReactionAdd.splice(-1, 1);
+                                msg.channel.createMessage("Reactions timed out, user not thrown in detention")
+                            }
+                        }, 15 * 1000)
                     })
                 })
             })
 
-            setTimeout(() => {
-                if (reacted == false) {
-                    bot._events.messageReactionAdd.splice(-1, 1);
-                    msg.channel.createMessage("Reactions timed out, user not thrown in detention")
-                }
-            }, 15 * 1000)
+            
 
         } else {
             return "Warning recorded <:bexy:393137089622966272>";
