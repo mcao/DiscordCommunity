7fa0c984ace3d6001ad968a4ecdb8569bf334a33
Michael Cao <mike.x.cao@gmail.com>
2018-01-04 16:48:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -47,48 +47,49 @@ bot.on("ready", () => {
 
 bot.on("messageCreate", function(msg) {
     if (msg.channel.type == 1) {
-        bot.createMessage(msg.channel.id, 'hey dude im not working rn go away');
-    }
-    const reactions = ['#⃣', '🇭', '🇾', '🇵', '🇪', '✨', 'bexhype:390557755339177994', 'bexlove:390556541717053440', 'bexhey:390556541360799748', 'bexangry:390557738473881601', 'hypekey:390416915207815168', 'nitro:390416828272476161', 'love:390416915194970122', 'HypeMan:390416914826133505', 'wlove:390416915341901826'];
-    const voteReactions = ['bexy:393137089622966272', 'bexn:393137089631354880'];
-    const channels = ["392407095171088384", "392173071223750656", "392172869154635786", "392173094728630275"];
+        bot.createMessage(msg.author.id, 'hey dude im not working rn go away');
+    } else {
+        const reactions = ['#⃣', '🇭', '🇾', '🇵', '🇪', '✨', 'bexhype:390557755339177994', 'bexlove:390556541717053440', 'bexhey:390556541360799748', 'bexangry:390557738473881601', 'hypekey:390416915207815168', 'nitro:390416828272476161', 'love:390416915194970122', 'HypeMan:390416914826133505', 'wlove:390416915341901826'];
+        const voteReactions = ['bexy:393137089622966272', 'bexn:393137089631354880'];
+        const channels = ["392407095171088384", "392173071223750656", "392172869154635786", "392173094728630275"];
 
-    if (msg.channel.id == '392407095171088384') {
-        if (bot.reactions.length == 0) return;
-        bot.reactions.forEach(function(reaction) {
-            msg.addReaction(reaction);
-        });
-    }
+        if (msg.channel.id == '392407095171088384') {
+            if (bot.reactions.length == 0) return;
+            bot.reactions.forEach(function(reaction) {
+                msg.addReaction(reaction);
+            });
+        }
 
-    if (msg.channel.id == "392152654505050112") { // #introductions
-        msg.addReaction('bexhey:390556541360799748');
-    } else if (channels.indexOf(msg.channel.id) > -1) {
-        if (msg.content.toLowerCase().includes('poll')) return;
-        for (var reaction in reactions) {
-            msg.addReaction(reaction);
+        if (msg.channel.id == "392152654505050112") { // #introductions
+            msg.addReaction('bexhey:390556541360799748');
+        } else if (channels.indexOf(msg.channel.id) > -1) {
+            if (msg.content.toLowerCase().includes('poll')) return;
+            for (var reaction in reactions) {
+                msg.addReaction(reaction);
+            }
         }
-    }
-    if (msg.content.toLowerCase().startsWith('suggestion:') && msg.channel.id === '392178846306402314') { // #staff-feedback
-        voteReactions.forEach(function(vote) {
-            msg.addReaction(vote);
-        });
-    }
-    if (!msg.author.bot && msg.channel.guild.id == HOME_GUILD) {
-        if (!bot.cooldowns.has(msg.author.id)) {
-            bot.cooldowns.add(msg.author.id)
-            bot.incrementMessage(msg)
-            setTimeout(() => {
-                bot.cooldowns.delete(msg.author.id);
-            }, 10 * 1000)
+        if (msg.content.toLowerCase().startsWith('suggestion:') && msg.channel.id === '392178846306402314') { // #staff-feedback
+            voteReactions.forEach(function(vote) {
+                msg.addReaction(vote);
+            });
         }
-    }
-    if (msg.channel.id == "397522914955755531" && msg.author.id == "392445621165883392") {
-        bot.createMessage("392442695756546059", "Automatic Code Update Initiated.").then(e => { // #bot-development
-            var evaled = require("child_process").execSync('git pull').toString()
-            bot.createMessage("392442695756546059", "Automatic Code Update Successful.")
-            var e = msg.embeds[0].description.toString()
-            bot.createMessage("392442695756546059", `<@171319044715053057>, the following changes were pushed by **${e.match(/.+\s-\s([\w\d-_]+)$/)[1] || "Unknown"}**. Please approve the changes and restart the bot.\n\`\`\`${evaled}\`\`\``)
-        })
+        if (!msg.author.bot && msg.channel.guild.id == HOME_GUILD) {
+            if (!bot.cooldowns.has(msg.author.id)) {
+                bot.cooldowns.add(msg.author.id)
+                bot.incrementMessage(msg)
+                setTimeout(() => {
+                    bot.cooldowns.delete(msg.author.id);
+                }, 10 * 1000)
+            }
+        }
+        if (msg.channel.id == "397522914955755531" && msg.author.id == "392445621165883392") {
+            bot.createMessage("392442695756546059", "Automatic Code Update Initiated.").then(e => { // #bot-development
+                var evaled = require("child_process").execSync('git pull').toString()
+                bot.createMessage("392442695756546059", "Automatic Code Update Successful.")
+                var e = msg.embeds[0].description.toString()
+                bot.createMessage("392442695756546059", `<@171319044715053057>, the following changes were pushed by **${e.match(/.+\s-\s([\w\d-_]+)$/)[1] || "Unknown"}**. Please approve the changes and restart the bot.\n\`\`\`${evaled}\`\`\``)
+            })
+        }
     }
 })
 
