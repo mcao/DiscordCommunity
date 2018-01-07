8dc5e4e2a88c3c8319e98799dd6da01d30bc25ae
Unknown <justmesaif@yahoo.com>
2018-01-07 09:41:38 -0500

    changed autorole to 2 mins

--- index.js
+++ index.js
@@ -369,7 +369,7 @@ bot.on("guildMemberAdd", function (guild, member) {
 Please remember to read the <#392171939101409290> and post something in <#392152654505050112> if you'd like! <:bexlove:390556541717053440>`)
         setTimeout(function () {
             member.addRole('392169263982444546', "Autorole")
-        }, 300000);
+        }, 120000);
     }
 })
 
