712bb3aab77a11d99fb72f1c785e4f44aa4840ab
Unknown <justmesaif@yahoo.com>
2018-01-08 09:49:20 -0500

    bye autorole

--- index.js
+++ index.js
@@ -305,9 +305,9 @@ bot.on("guildMemberAdd", function (guild, member) {
     if (guild.id == HOME_GUILD) {
         bot.createMessage("392152516596465664", `Welcome to the official Discord Hub Community, <@${member.user.id}>! :tada::tada:
 Please remember to read the <#392171939101409290> and post something in <#392152654505050112> if you'd like! <:bexlove:390556541717053440>`)
-        setTimeout(function () {
-                member.addRole('392169263982444546', "Autorole").catch(() => {});
-        }, 120000);
+        // setTimeout(function () {
+        //         member.addRole('392169263982444546', "Autorole").catch(() => {});
+        // }, 120000);
         member.user.getDMChannel().then((channel) => {
             // Community banner
             channel.createMessage("", { file: fs.readFileSync("./images/community.png"), name: "welcome.png" }).then((msg) => {
