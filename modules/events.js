69c01f4ee7e10f14a18c9c203ff4c1ac2308d2b8
JoeBanks13 <joseph@josephbanks.me>
2018-01-20 13:15:21 +0000

    leave queue command

--- modules/events.js
+++ modules/events.js
@@ -1,10 +1,18 @@
 module.exports = bot => {
     bot.registerCommand('queue', msg => {
         if (bot.queue.indexOf(msg.author.id) !== -1) {
-            return `:ok_hand: You are position #${bot.queue.indexOf(msg.author.id) + 1}`;
+            return `<:bexy:393137089622966272> You are position #${bot.queue.indexOf(msg.author.id) + 1}`;
         } else {
             bot.queue.push(msg.author.id);
-            return `:ok_hand: You are now position #${bot.queue.indexOf(msg.author.id) + 1} in the queue`;
+            return `<:bexy:393137089622966272> You are now position #${bot.queue.indexOf(msg.author.id) + 1} in the queue`;
         }
+    });
+
+    bot.registerCommand('leavequeue', msg => {
+        if (bot.queue.indexOf(msg.author.id) === -1) return '<:bexn:393137089631354880> You were never in the queue!';
+
+        let index = bot.queue.indexOf(msg.author.id);
+        bot.queue.splice(index, 1);
+        return '<:bexy:393137089622966272> You have been removed from the queue';
     });
 };
